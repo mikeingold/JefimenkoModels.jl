843e4a14bfa9e8fa2ba071ff42ed7cd340048d05
Michael Ingold <mike.ingold@gmail.com>
2023-04-03 12:47:26 -0400

    Implement some basic tests

---
+++ test/runtests.jl
@@ -0,0 +1,18 @@
+using Unitful, UnitfulCoordinateSystems
+using Unitful.DefaultSymbols: m, ns
+using JefimenkoModels, Test
+
+@testset "Accessors for LineSource_Straight" begin
+    a = CoordinateCartesian(-0.5m, 0.0m, 0.0m)
+    b = CoordinateCartesian( 0.5m, 0.0m, 0.0m)
+    J(r̄::AbstractCoordinate, t_s::Real) = x̂ .* cos(2π*100e6*t_s)
+    rho(r̄::AbstractCoordinate, t_s::Real) = x̂ .* cos(2π*100e6*t_s)^2
+    source = LineSource_Straight{Float64}(a, b, rho, rho, J, J)
+
+	@test source.a.x ≈ source.ā.x ≈ -0.5m
+    @test source.b.x ≈ source.b̄.x ≈  0.5m
+    @test source.rho_e === source.ρₑ === source.ρe
+    @test source.rho_h === source.ρₕ === source.ρh
+    @test source.J_e === source.Je === source.Jₑ
+    @test source.J_h === source.Jh === source.Jₕ
+end
