06a8eead85f94c55a106d12f91cd9ec187af6916
Michael Ingold <mike.ingold@gmail.com>
2023-07-04 16:35:31 -0400

    Update links and add docs analytics

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,17 @@ push!(LOAD_PATH, "../src/")
 
 using Documenter, JefimenkoModels
 
-makedocs(sitename="JefimenkoModels.jl",
-	 authors = "Michael Ingold",
-	 pages = [
-		"Home" => "index.md",
-		"Tutorial" => "tutorial.md",
-		"Reference" => "reference.md"
-	 ]
+makedocs(
+    sitename="JefimenkoModels.jl",
+    authors = "Michael Ingold",
+    format = Documenter.HTML(analytics = "G-6SFXR709Z9",
+			     canonical = "https://mikeingold.github.io/JefimenkoModels.jl/stable/"),
+    pages = [
+            "Home" => "index.md",
+            "Tutorial" => "tutorial.md",
+            "Reference" => "reference.md"
+	    ]
 	)
 
-deploydocs(repo="github.com/mikeingold/JefimenkoModels.jl")
+deploydocs(repo="github.com/mikeingold/JefimenkoModels.jl.git")
 
