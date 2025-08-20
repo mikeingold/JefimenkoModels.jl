11d170470cfd137bbc92ba69ce9d2c18b4933a14
Michael Ingold <mike.ingold@gmail.com>
2025-08-20 17:00:11 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,20 @@
-using Unitful, UnitfulCoordinateSystems
+using Unitful
 using Unitful.DefaultSymbols: m, ns
-using JefimenkoModels, Test
+using UnitfulCoordinateSystems
+using JefimenkoModels
+using Test
 
 @testset "Accessors for LineSource_Straight" begin
     a = CoordinateCartesian(-0.5m, 0.0m, 0.0m)
     b = CoordinateCartesian( 0.5m, 0.0m, 0.0m)
-    rho(r̄::AbstractCoordinate, t_s::Real) = cos(2π*100e6*t_s)^2
-    J(r̄::AbstractCoordinate, t_s::Real) = x̂ .* cos(2π*100e6*t_s)
+    rho(r̄::AbstractCoordinate, t_s::Real) = cos(2π * 100e6 * t_s)^2
+    J(r̄::AbstractCoordinate, t_s::Real) = x̂ .* cos(2π * 100e6 * t_s)
     source = LineSource_Straight{Float64}(a, b, rho, rho, J, J)
 
-	@test source.a.x ≈ source.ā.x ≈ -0.5m
-    @test source.b.x ≈ source.b̄.x ≈  0.5m
-    @test source.rho_e === source.ρₑ === source.ρe
-    @test source.rho_h === source.ρₕ === source.ρh
-    @test source.J_e === source.Je === source.Jₑ
-    @test source.J_h === source.Jh === source.Jₕ
+	@test source.a.x ≈ -0.5m
+    @test source.b.x ≈  0.5m
+    @test source.rho_e === source.ρₑ
+    @test source.rho_h === source.ρₕ
+    @test source.J_e === source.Je
+    @test source.J_h === source.Jh
 end
